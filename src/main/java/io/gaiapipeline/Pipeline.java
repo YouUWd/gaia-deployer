a4a8235eefafb93cfd7f3dbde95c626c68a55fbc
youyou.dyy <youyou.dyy@alibaba-inc.com>
2019-05-16 19:52:18 +0800

    vault

--- src/main/java/io/gaiapipeline/Pipeline.java
+++ src/main/java/io/gaiapipeline/Pipeline.java
@@ -6,27 +6,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.logging.Logger;
 
-public class Pipeline {
+public class Pipeline
+{
 	private static final Logger LOGGER = Logger.getLogger(Pipeline.class.getName());
 
 	private static Handler MyAwesomeJob = (gaiaArgs) -> {
-		for (PipelineArgument arg : gaiaArgs) {
+		for (PipelineArgument arg: gaiaArgs) {
 			LOGGER.info("Key: " + arg.getKey() + "; Value: " + arg.getValue());
 		}
 	};
 
-	public static void main(String[] args) {
+	public static void main( String[] args )
+	{
 		PipelineJob myjob = new PipelineJob();
 		myjob.setTitle("MyAwesomeJob");
 		myjob.setDescription("Do something awesome.");
 		myjob.setHandler(MyAwesomeJob);
-		PipelineArgument argUsername = new PipelineArgument();
-		// Instead of InputType.TextFieldInp you can also use InputType.TextAreaInp
-		// for a text area or InputType.BoolInp for boolean input.
-		argUsername.setType(InputType.TextFieldInp);
-		argUsername.setKey("username");
-		argUsername.setDescription("Please provide the username:");
-		myjob.setArgs(new ArrayList<>(Arrays.asList(argUsername)));
+		PipelineArgument vaultParam = new PipelineArgument();
+		vaultParam.setType(InputType.VaultInp);
+		vaultParam.setKey("dbpassword");
+		myjob.setArgs(new ArrayList<>(Arrays.asList(vaultParam)));
 
 		Javasdk sdk = new Javasdk();
 		try {
