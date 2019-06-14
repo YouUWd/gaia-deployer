780e917c6e19542ace301aa360ee1d90337ae41c
youyou.dyy <youyou.dyy@alibaba-inc.com>
2019-06-14 10:26:57 +0800

    test arg

--- src/main/java/utils/ShellUtil.java
+++ src/main/java/utils/ShellUtil.java
@@ -4,8 +4,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.logging.Logger;
 
 public class ShellUtil {
+
+	private static final Logger LOGGER = Logger.getLogger(ShellUtil.class.getName());
 
 	public static void exec(String cmd) throws Exception {
 		ProcessBuilder builder = new ProcessBuilder();
@@ -13,7 +16,7 @@ public class ShellUtil {
 		builder.directory(new File(System.getProperty("user.home")));
 		Process process = builder.start();
 		InputStream inputStream = process.getInputStream();
-		new BufferedReader(new InputStreamReader(inputStream)).lines().forEach(System.out::println);
+		new BufferedReader(new InputStreamReader(inputStream)).lines().forEach(LOGGER::info);
 		int exitCode = process.waitFor();
 		process.destroy();
 		assert exitCode == 0;
